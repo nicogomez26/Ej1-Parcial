65209e14f7ec678fdb1f0d6e60ef9f02dffff80a
nicogomez26 <Nicolas.Gomez2@alumnos.uai.edu.ar>
2025-10-15 21:25:55 -0300

    agregando clases

---
+++ ParcialT1/BE/Lote.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Lote
+    {
+		private int nroLote;
+
+		public int NroLote
+		{
+			get { return nroLote; }
+			set { nroLote = value; }
+		}
+
+		private string nombreProp;
+
+		public string NombreProp
+        {
+			get { return nombreProp; }
+			set { nombreProp = value; }
+		}
+
+		private double medidaFrente;
+
+		public double MedidaFrente
+		{
+			get { return medidaFrente; }
+			set { medidaFrente = value; }
+		}
+
+		private double medidaFondo;
+
+		public double MedidaFondo
+		{
+			get { return medidaFondo; }
+			set { medidaFondo = value; }
+		}
+
+		private double totalMts2;
+			
+		public double TotalMts2
+		{
+			get { return totalMts2; }
+			set { totalMts2 = value; }
+		}
+
+
+
+	}
+}
---
+++ ParcialT1/BE/Pago.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Pago
+    {
+
+		private int nroPago;
+
+		public int NroPago
+		{
+			get { return nroPago; }
+			set { nroPago = value; }
+		}
+
+		private int nroLote;
+
+		public int NroLote
+		{
+			get { return nroLote; }
+			set { nroLote = value; }
+		}
+
+		private int ubicacion;
+
+		public int Ubicacion
+		{
+			get { return ubicacion; }
+			set { ubicacion = value; }
+		}
+
+		private double mts2Total;
+
+		public double Mts2Total
+		{
+			get { return mts2Total; }
+			set { mts2Total = value; }
+		}
+
+		private double precioMts2;
+
+		public double PrecioMts2
+		{
+			get { return precioMts2; }
+			set { precioMts2 = value; }
+		}
+
+		private double total;
+
+		public double Total
+		{
+			get { return total; }
+			set { total = value; }
+		}
+
+	}
+}
